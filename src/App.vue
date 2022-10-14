e717eadda1839e3d83cb6f6a5baebc1b640f70fb
Eric <allistirking422@gmail.com>
2022-10-15 01:12:20 +0300

    initialize

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+const tg = window.Telegram.WebApp
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background: var(--tg-theme-bg-color);
+  color: var(--tg-theme-text-color);
 }
 </style>
