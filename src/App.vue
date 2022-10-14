5a3dc96b3cc6b3e88f6cac1ceb2a6952c63a16ef
Eric <allistirking422@gmail.com>
2022-10-15 01:58:27 +0300

    init-02

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="main">
+    <div class="content">
+      <div class="from-content">
+        <h3>Registration</h3>
+        <form action="" method="POST">
+          <input type="text" placeholder="name">
+          <input type="text" placeholder="email">
+          <input type="text" placeholder="password">
+          <select name="Area" id="">
+            <option value="US"></option>
+            <option value="ES"></option>
+            <option value="Asia"></option>
+          </select>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
