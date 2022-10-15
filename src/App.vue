77e771a852a9bb73ea5a29fd9b15046fb3bc56ba
Eric <allistirking422@gmail.com>
2022-10-16 00:06:22 +0300

    init-05

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,11 @@
           <input type="text" placeholder="name">
           <input type="text" placeholder="email">
           <input type="text" placeholder="password">
-          <select name="Area" id="Area">
-            <option value="US">US</option>
-            <option value="ES">US</option>
-            <option value="Asia">US</option>
-          </select>
+<!--          <select name="Area" id="Area">-->
+<!--            <option value="US">US</option>-->
+<!--            <option value="ES">US</option>-->
+<!--            <option value="Asia">US</option>-->
+<!--          </select>-->
         </form>
       </div>
     </div>
@@ -19,9 +19,13 @@
 </template>
 
 <script>
-// const tg = window.Telegram.WebApp
+const tg = window.Telegram.WebApp
 export default {
   name: 'App',
+  mounted() {
+    tg.MainButton.show()
+  }
+
 }
 </script>
 
@@ -29,5 +33,42 @@ export default {
 body {
   background: var(--tg-theme-bg-color);
   color: var(--tg-theme-text-color);
+}
+
+.main {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+form {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+form input {
+  background: none;
+  border: 1px solid gray;
+  margin-top: 10px;
+  width: 500px;
+  height: 20px;
+  padding: 5px;
+}
+
+form select {
+  background: none;
+  border: 1px solid gray;
+  margin-top: 10px;
+  width: 500px;
+  height: 30px;
+  padding: 5px;
+}
+
+form select option {
+
 }
 </style>
